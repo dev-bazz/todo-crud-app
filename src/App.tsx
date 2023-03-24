14843d5481d6d48ef223d9f3a22e25de271abed5
bazz <clement.f.bazz@gmail.com>
2023-03-24 19:43:31 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import "./style.scss"
+
 export default function App() {
+  const [state, setState] = useState(false)
 
   return (
     <div className="App">
       <h1>Hello World</h1>
+
+      <div className={state ? `item show` : ` item`}>
+        {state ? <ul>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+          <li>Item 1</li>
+        </ul> : ""}
+      </div>
+      <button onClick={() => setState(!state)}> Check Me</button>
     </div>
   )
+
+
 }
 
 
