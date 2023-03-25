754d1f5956eb4f5c187fe47d21f599f947fe90c1
bazz <clement.f.bazz@gmail.com>
2023-03-25 12:47:09 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,46 @@ import "./style.scss"
 export default function App() {
   const [state, setState] = useState(false)
 
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
+  const items = [
+    {
+      title: `Nav Name`,
+      children: [{ content: `This is an Item` }, { content: `this is another item` }]
+    },
+    {
+      title: `Nav Name`,
+      children: [{ content: `This is an Item` }, { content: `this is another item` }]
+    }
+  ]
 
-      <div className={state ? `item show` : ` item`}>
-        {state ? <ul>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-          <li>Item 1</li>
-        </ul> : ""}
+  const sideNav = []
+
+
+  return (
+    <>
+      <div className="App">
+        <h1>Hello World</h1>
+
+        <div className="accord">
+          {items.map((item, key) => {
+            return (<details key={key}>
+              <summary>{item.title}</summary>
+              <ul>
+                {item.children.map((child, key) => <li key={key}>{child.content}</li>)}
+              </ul>
+            </details>)
+          })}
+        </div>
+        <button style={{
+          marginTop: "1rem",
+          backgroundColor: "black",
+          color: "white",
+          padding: "1rem 2rem",
+          borderRadius: "8px"
+        }} onClick={() => setState(!state)}> Check Me</button>
+
+
       </div>
-      <button onClick={() => setState(!state)}> Check Me</button>
-    </div>
+    </>
   )
 
 
